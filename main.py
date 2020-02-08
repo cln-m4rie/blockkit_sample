220d51d07814532c6f83b0ca6d88ce00651464ef
cln-m4rie <s.kikkawa0613@gmail.com>
2020-02-08 17:37:10 +0900

    update

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import requests
 from envs import WEBHOOK_URL
 
 
-def post(data: dict) -> requests.Response:
+def send2slack(data: dict) -> requests.Response:
     res = requests.post(WEBHOOK_URL, json=data, headers={
-                        'Content-Type': 'application/json'})
+        "Content-Type": "application/json"
+    })
     return res
 
 
 def fetch_repos():
-    res = requests.get('https://api.github.com/users/cln-m4rie/repos')
+    res = requests.get("https://api.github.com/users/cln-m4rie/repos")
     res.raise_for_status()
     return res.json()
 
@@ -20,36 +21,34 @@ def main():
 
     blocks = [
         {
-            'type': 'section',
+            "type": "section",
             'text': {
-                'type': 'mrkdwn',
-                'text': 'GitHub API',
+                "type": "mrkdwn",
+                "text": "GitHub API",
             }
         },
-        {
-            'type': 'divider'
-        },
     ]
+
     for repo in repos:
+        blocks.append({
+            "type": "divider"
+        })
         blocks.append(
             {
-                'type': 'section',
-                'text': {
-                    'type': 'mrkdwn',
-                    'text': f"*{repo['name']}*\n"
-                    f"Language: {repo['language']}\n"
-                    f"Star: {repo['stargazers_count']}\n"
-                    f"Last updated {repo['updated_at']}\n"
-                    f"{repo['description']}\n"
-                    f"<{repo['html_url']}|View this repository on GitHub>"
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"*{repo['name']}*\n"
+                            f"Language: {repo['language']}\n"
+                            f"Star: {repo['stargazers_count']}\n"
+                            f"Last updated {repo['updated_at']}\n"
+                            f"{repo['description']}\n"
+                            f"<{repo['html_url']}|View this repository on GitHub>"
                 },
             },
         )
-        blocks.append({
-            'type': 'divider'
-        })
-    post({'blocks': blocks})
-
+    r = send2slack({"blocks": blocks})
+    print(r.content)
 
 if __name__ == "__main__":
     main()
